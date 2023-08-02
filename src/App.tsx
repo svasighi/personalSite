ffdee5a8679571d0e0fc0c9d42dd3544f911a3c1
shadmehr vasighi <shadmehr.vasighi99@gmail.com>
2023-08-03 00:08:58 +0330

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -26,13 +26,13 @@ function App() {
   }, []);
 
   return (
-    <div className="container flex space-x-7 justify-between py-16 h-screen box-border mx-auto">
+    <div className="container flex space-x-7 justify-between h-screen box-border mx-auto">
       <div>
         <div className="w-16  h-16 absolute rounded-full bg-pink-500 right-8 top-20 -z-10 blur-3xl"></div>
         <div className="w-28 h-28 absolute rounded-full  bg-yellow-500 -z-10 right-32 -top-12 blur-3xl"></div>
       </div>
       {/* picture */}
-      <div className="w-96 pb-12 relative">
+      <div className="w-96 pb-12 mb-16 pt-16 relative">
         <div className="flex flex-col items-stretch relative h-full bg-amber-100 border border-slate-800 rounded-2xl  justify-between before:bg-grid before:w-full before:bg-repeat before:content-[''] before:h-full before:absolute overflow-hidden">
           <div className="flex justify-between pt-10 px-7 text-gray-900">
             <div className=" font-bold leading-9 text-3xl text-left">
@@ -96,7 +96,7 @@ function App() {
         </div>
       </div>
       {/* sidebar */}
-      <div className="flex flex-col w-6">
+      <div className="flex flex-col w-6 ">
         <div className="flex flex-col items-center space-x-2 ">
           <p className="">webpack</p>
           <img className="w-6" src="/stack/webpack.svg" alt="logo" />
@@ -108,7 +108,7 @@ function App() {
       </div>
       <div
         ref={ref}
-        className="flex-1 text-gray-900 h-auto text-white overflow-auto">
+        className=" bg-slate-200 flex-1 text-gray-700 px-12 py-8 mt-16 h-auto rounded-xl text-justify  overflow-auto">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam atque
         quisquam asperiores expedita dolorem. Vero, laudantium ullam autem
         laborum asperiores harum, adipisci nihil laboriosam repellendus dicta
