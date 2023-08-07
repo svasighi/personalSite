d3e4118b015d26bef743a8acd614d31c2b173e2e
shadmehr vasighi <shadmehr.vasighi99@gmail.com>
2023-08-07 22:00:49 +0330

    fix experience styling

--- src/sections/Experience/index.tsx
+++ src/sections/Experience/index.tsx
@@ -10,28 +10,18 @@ const Experience = () => {
           return (
             <div className="group flex space-x-4" key={experience.title}>
               <div
-                className={classNames("flex w-[22px] flex-col items-center", {
-                  "items-end": i === 0,
-                  "items-start": i != 0,
-                })}
+                className={classNames("flex w-[22px] flex-col items-center ")}
               >
-                {i != 0 && (
-                  <div className="w-1 flex-1 items-center border-l border-dashed border-gray-400"></div>
-                )}
                 <div
                   className={classNames(
                     " h-6 w-6 rounded-full bg-amber-200 transition-colors duration-500 ease-in-out group-hover:bg-amber-400",
-                    {
-                      "mt-12": i == 0,
-                      "mb-12": i + 1 == MY_EXPERIENCES.length,
-                    },
                   )}
                 ></div>
                 {i + 1 != MY_EXPERIENCES.length && (
-                  <div className="w-1 flex-1 border-l border-dashed border-gray-400"></div>
+                  <div className="w-0 flex-1 border-l border-dashed border-gray-400"></div>
                 )}
               </div>
-              <div className="my-3.5 flex flex-1 flex-col space-y-2 group-first:mt-4 group-last:mb-4">
+              <div className="mb-3.5 flex flex-1 flex-col space-y-2">
                 <div className=" text-base font-semibold leading-normal text-gray-700">
                   {experience.title}
                   <span className="text-sm font-medium leading-tight text-gray-400">
